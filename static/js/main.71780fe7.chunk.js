0687ce7aa66fc06a9f448f66c19317e566bb6043
yuriykolisnyk <ykolisnyk@gmail.com>
2021-12-08 15:48:26 +0200

    Updates

---
+++ static/js/main.71780fe7.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-01-components"]=this["webpackJsonpgoit-react-hw-01-components"]||[]).push([[0],{3:function(e,o,t){"use strict";t.r(o);var n=t(0),c=t.n(n),r=t(2),s=t.n(r),a=c.a.createElement("div",{a:5,b:10,children:"\u041f\u0440\u0438\u0432\u0435\u0442 \u043c\u0438\u0440"});console.log(a),s.a.render(a,document.querySelector("#root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.71780fe7.chunk.js.map
